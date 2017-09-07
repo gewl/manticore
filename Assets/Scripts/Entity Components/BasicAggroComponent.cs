ec3047f53424ac8da7791171e4a8da174d42fabb
gewl <gilbertwlawson@gmail.com>
2017-09-06 22:55:36 -0400

    Added GameManager

---
+++ Assets/Scripts/Entity Components/BasicAggroComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicAggroComponent : EntityComponent {
+    [SerializeField]
+    float aggroRange = 0f;
+
+    void Awake()
+    {
+        base.Awake();
+    }
+
+    public override void Initialize()
+    {
+    }
+
+    public override void Cleanup()
+    {
+    }
+}
---
+++ Assets/Scripts/Globals/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private static GameManager instance = null;
+
+    public static GameManager Instance
+    {
+        get 
+        {
+            if (instance == null)
+            {
+                GameObject managerObject = new GameObject();
+                instance = managerObject.AddComponent<GameManager>();
+                managerObject.name = "Game Manager Object";
+            }
+            return instance;
+        }
+    }
+
+    GameObject playerObject;
+    Transform playerTransform;
+
+    void Awake()
+    {
+        playerObject = GameObject.Find("Manticore");
+        playerTransform = playerObject.transform;
+    }
+
+    public Vector3 GetPlayerPosition()
+    {
+        return playerTransform.position;
+    }
+}
