aaed4a11e6099a3d19a7de60a4ed7edd09b7ea6a
gewl <gilbertwlawson@gmail.com>
2017-09-30 19:49:56 -0400

    Moving unaggroed units now face next waypoint

---
+++ Assets/Scripts/Entity Components/Health Components/StationaryEntityHealthComponent.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationaryEntityHealthComponent : EntityComponent
+{
+
+    [SerializeField]
+    float currentHealth;
+    [SerializeField]
+    float recoveryTime;
+    [SerializeField]
+    float timeToDie;
+    [SerializeField]
+    Material damagedSkin;
+    [SerializeField]
+    Material deadSkin;
+    [SerializeField]
+    Material flashSkin;
+    [SerializeField]
+    Material darkFlashSkin;
+
+    [SerializeField]
+    Transform floatingDamageText;
+    [SerializeField]
+    Transform hud;
+
+    MeshRenderer meshRenderer;
+    Camera mainCamera;
+
+    float currentRecoveryTimer;
+    float currentDeathTimer;
+    Material originalSkin;
+    bool isEnabled = false;
+
+    enum Allegiance { Friendly, Enemy }
+    [SerializeField]
+    Allegiance entityAllegiance = Allegiance.Enemy;
+    bool isInvulnerable = false;
+    float initialHealth;
+
+    #region accessors
+    public float CurrentHealth()
+    {
+        return currentHealth;
+    }
+
+    public float InitialHealth()
+    {
+        if (initialHealth == 0)
+        {
+            initialHealth = currentHealth;
+        }
+        return initialHealth;
+    }
+    #endregion
+
+    protected void OnEnable()
+    {
+        isEnabled = true;
+        initialHealth = currentHealth;
+        mainCamera = Camera.main;
+    }
+
+    protected override void Subscribe()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+
+        entityEmitter.SubscribeToEvent(EntityEvents.Invulnerable, OnInvulnerable);
+        entityEmitter.SubscribeToEvent(EntityEvents.Vulnerable, OnVulnerable);
+    }
+
+    protected override void Unsubscribe()
+    {
+        entityEmitter.UnsubscribeFromEvent(EntityEvents.Invulnerable, OnInvulnerable);
+        entityEmitter.UnsubscribeFromEvent(EntityEvents.Vulnerable, OnVulnerable);
+    }
+
+    void OnInvulnerable()
+    {
+        isInvulnerable = true;
+    }
+
+    void OnVulnerable()
+    {
+        isInvulnerable = false;
+    }
+
+    public void OnCollisionEnter(Collision projectile)
+    {
+        if (!isEnabled)
+        {
+            return;
+        }
+        if (DoesBulletDamage(projectile.gameObject) && !isInvulnerable)
+        {
+            BasicBullet bullet = projectile.transform.GetComponent<BasicBullet>();
+            float damage = bullet.strength;
+            currentHealth -= damage;
+
+            Vector3 damageTextPosition = mainCamera.WorldToScreenPoint(transform.position);
+            damageTextPosition.y += 15f;
+            Transform instantiatedDamageText = Instantiate(floatingDamageText, damageTextPosition, Quaternion.identity, hud);
+            instantiatedDamageText.GetComponent<FloatingDamageText>().DamageValue = damage;
+
+            isInvulnerable = true;
+            if (currentHealth > 0)
+            {
+                Damage(projectile);
+            }
+            else
+            {
+                Die(projectile);
+            }
+        }
+    }
+
+    bool DoesBulletDamage(GameObject bullet)
+    {
+        if (entityAllegiance == Allegiance.Enemy)
+        {
+            return bullet.CompareTag("FriendlyBullet");
+        }
+        else if (entityAllegiance == Allegiance.Friendly)
+        {
+            return bullet.CompareTag("EnemyBullet");
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void Damage(Collision damagingProjectileCollision)
+    {
+        // Announce hurt; subscribe to handle timer, lerping material, etc.
+        entityEmitter.EmitEvent(EntityEvents.Stun);
+
+        // Knock back
+        Vector3 collisionVelocity = damagingProjectileCollision.relativeVelocity;
+        entityData.EntityRigidbody.velocity = collisionVelocity;
+
+        entityData.EntityRigidbody.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+        entityData.EntityRigidbody.AddTorque(0f, Mathf.Sqrt(Mathf.Abs(collisionVelocity.x * collisionVelocity.z)), 0f);
+
+        // Initialize timer from set values
+        currentRecoveryTimer = recoveryTime;
+
+        meshRenderer.material = flashSkin;
+        // Store original skin for lerping
+        originalSkin = meshRenderer.material;
+
+        StartCoroutine("DamagedProcess");
+    }
+
+    void Die(Collision killingProjectileCollision)
+    {
+        entityEmitter.EmitEvent(EntityEvents.Dead);
+
+        // Knock back
+        Vector3 collisionVelocity = killingProjectileCollision.relativeVelocity;
+        entityData.EntityRigidbody.velocity = collisionVelocity;
+
+        entityData.EntityRigidbody.constraints = RigidbodyConstraints.None;
+        collisionVelocity.Normalize();
+
+        // Initialize timer from set values
+        currentDeathTimer = timeToDie;
+
+        meshRenderer.material = darkFlashSkin;
+        // Store original skin for lerping
+        originalSkin = meshRenderer.material;
+
+        StartCoroutine("DyingProcess");
+
+    }
+
+    IEnumerator DamagedProcess()
+    {
+        entityEmitter.EmitEvent(EntityEvents.HealthChanged);
+        while (true)
+        {
+            if (currentRecoveryTimer > 0f)
+            {
+                float skinTransitionCompletion = (recoveryTime - currentRecoveryTimer) / recoveryTime;
+                meshRenderer.material.Lerp(originalSkin, damagedSkin, skinTransitionCompletion);
+
+                currentRecoveryTimer -= Time.deltaTime;
+                yield return new WaitForFixedUpdate();
+            }
+            else
+            {
+                meshRenderer.material = damagedSkin;
+                entityEmitter.EmitEvent(EntityEvents.Unstun);
+                isInvulnerable = false;
+                yield break;
+            }
+        }
+    }
+
+    IEnumerator DyingProcess()
+    {
+        entityEmitter.EmitEvent(EntityEvents.HealthChanged);
+        while (true)
+        {
+            if (currentDeathTimer > 0f)
+            {
+                float skinTransitionCompletion = (timeToDie - currentDeathTimer) / timeToDie;
+                skinTransitionCompletion = Mathf.Sqrt(skinTransitionCompletion);
+                meshRenderer.material.Lerp(originalSkin, deadSkin, skinTransitionCompletion);
+
+                currentDeathTimer -= Time.deltaTime;
+                yield return new WaitForFixedUpdate();
+            }
+            else
+            {
+                meshRenderer.material = deadSkin;
+                entityData.EntityRigidbody.detectCollisions = false;
+                entityData.EntityRigidbody.drag = 10f;
+                entityData.EntityRigidbody.freezeRotation = true;
+                entityData.EntityRigidbody.AddForce(new Vector3(0f, -100, 0f), ForceMode.Impulse);
+                if (transform.position.y <= -4f)
+                {
+                    UnityEngine.Object.Destroy(gameObject);
+                }
+                yield return null;
+            }
+        }
+
+    }
+
+    IEnumerator Flash()
+    {
+        bool hasFlashed = false;
+        Material originalMaterial = meshRenderer.material;
+        while (true)
+        {
+            if (!hasFlashed)
+            {
+                meshRenderer.material = flashSkin;
+                hasFlashed = true;
+                yield return new WaitForSeconds(0.02f);
+            }
+            else
+            {
+                meshRenderer.material = originalMaterial;
+                yield break;
+            }
+        }
+    }
+
+    IEnumerator DarkFlash()
+    {
+        bool hasFlashed = false;
+        Material originalMaterial = meshRenderer.material;
+        while (true)
+        {
+            if (!hasFlashed)
+            {
+                meshRenderer.material = darkFlashSkin;
+                hasFlashed = true;
+                yield return new WaitForSeconds(0.05f);
+            }
+            else
+            {
+                meshRenderer.material = originalMaterial;
+                yield break;
+            }
+        }
+    }
+}
--- Assets/Scripts/Entity Components/Movement Components/RotateToFaceTarget.cs
+++ Assets/Scripts/Entity Components/Movement Components/RotateToFaceTarget.cs
@@ -8,10 +8,19 @@ public class RotateToFaceTarget : EntityComponent {
     [SerializeField]
     float rotationStrength;
     Transform currentTarget;
+    Vector3 nextWaypoint;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        nextWaypoint = Vector3.forward;
+    }
 
     protected override void Subscribe()
     {
         entityEmitter.SubscribeToEvent(EntityEvents.TargetUpdated, OnTargetUpdated);
+        entityEmitter.SubscribeToEvent(EntityEvents.Update, OnUpdate);
+        entityEmitter.SubscribeToEvent(EntityEvents.SetWaypoint, OnSetWaypoint);
         entityEmitter.SubscribeToEvent(EntityEvents.Stun, OnHurt);
         entityEmitter.SubscribeToEvent(EntityEvents.Dead, OnDead);
         entityEmitter.SubscribeToEvent(EntityEvents.Unstun, OnRecovered);
@@ -20,13 +29,16 @@ public class RotateToFaceTarget : EntityComponent {
     protected override void Unsubscribe()
     {
         entityEmitter.UnsubscribeFromEvent(EntityEvents.TargetUpdated, OnTargetUpdated);
+        entityEmitter.UnsubscribeFromEvent(EntityEvents.SetWaypoint, OnSetWaypoint);
         entityEmitter.UnsubscribeFromEvent(EntityEvents.Stun, OnHurt);
         entityEmitter.UnsubscribeFromEvent(EntityEvents.Dead, OnDead);
+        entityEmitter.UnsubscribeFromEvent(EntityEvents.Update, OnUpdate);
         entityEmitter.UnsubscribeFromEvent(EntityEvents.Unstun, OnRecovered);
-        if (currentTarget != null)
-        {
-            entityEmitter.UnsubscribeFromEvent(EntityEvents.Update, OnUpdate);
-        }
+    }
+
+    void OnSetWaypoint()
+    {
+        nextWaypoint = (Vector3)entityData.GetSoftAttribute(SoftEntityAttributes.NextWaypoint);
     }
 
     void OnDead()
@@ -37,17 +49,7 @@ public class RotateToFaceTarget : EntityComponent {
     void OnTargetUpdated()
     {
         Transform newTarget = (Transform)entityData.GetSoftAttribute(SoftEntityAttributes.CurrentTarget);
-
-        if (newTarget != null)
-        {
-            entityEmitter.SubscribeToEvent(EntityEvents.Update, OnUpdate);
-            currentTarget = newTarget;
-        }
-        else
-        {
-            currentTarget = null;
-            entityEmitter.UnsubscribeFromEvent(EntityEvents.Update, OnUpdate);
-        }
+        currentTarget = newTarget;
     }
 
     void OnHurt()
@@ -71,7 +73,16 @@ public class RotateToFaceTarget : EntityComponent {
 
     void Rotate()
     {
-        Quaternion targetRotation = Quaternion.LookRotation(currentTarget.position - transform.position);
+        Vector3 lookPosition;
+        if (currentTarget != null)
+        {
+            lookPosition = currentTarget.position;
+        }
+        else
+        {
+            lookPosition = nextWaypoint;
+        }
+        Quaternion targetRotation = Quaternion.LookRotation(lookPosition - transform.position);
         float str = Mathf.Min(rotationStrength * Time.deltaTime, 1);
         transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, str);
     }
