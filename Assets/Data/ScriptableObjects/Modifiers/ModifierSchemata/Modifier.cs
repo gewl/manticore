ee93784306dfcc64902662d0a3b05560a07c6d28
gewl <gilbertwlawson@gmail.com>
2018-02-21 16:23:14 -0500

    Added support for ParticleSystem to ValueModifiers, added SlowEffect to testslow

--- Assets/Data/ScriptableObjects/Modifiers/ModifierSchemata/Modifier.cs
+++ Assets/Data/ScriptableObjects/Modifiers/ModifierSchemata/Modifier.cs
@@ -7,6 +7,9 @@ public class Modifier : ScriptableObject {
     public ModifierType modifierType;
     public float baseDuration;
 
+    public GameObject modifierEffectParticles;
+    GameObject instantiatedParticles;
+
     EntityModifierHandler modifierHandler;
     float durationRemaining;
 
@@ -14,6 +17,15 @@ public class Modifier : ScriptableObject {
     {
         modifierHandler = _modifierHandler;
         durationRemaining = baseDuration;
+
+        Transform modifierTarget = modifierHandler.transform;
+
+        instantiatedParticles = Instantiate(modifierEffectParticles, modifierTarget.position, Quaternion.identity, modifierTarget);
+        ParticleSystem instantiatedParticleSystem = instantiatedParticles.GetComponent<ParticleSystem>();
+        ParticleSystem.MainModule mainParticleSystem = instantiatedParticleSystem.main;
+
+        mainParticleSystem.duration = baseDuration;
+        instantiatedParticleSystem.Play();
     }
 
     public void UpdateModifierDuration(float deltaTime)
