4e336b63beeb2e739aaf8021e906ad5e1b8a42c1
gewl <gilbertwlawson@gmail.com>
2018-03-13 18:30:40 -0400

    Highlighting clickable terms in dialogue windows

---
+++ Assets/Scripts/UI/Menus/DialogueMenu/DialogueBubbleController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class DialogueBubbleController : MonoBehaviour, IPointerClickHandler
+{
+    Text dialogueBubbleText;
+    TextGenerator generator;
+    float xDisplacement;
+
+    List<string> clickableTerms;
+
+    const string textContents = "The world is filled half with evil and half with good. We can tilt it forward so that more good runs into our minds, or back, so that more runs into this. But the quantities are the same, we change only their proportion here or there.";
+
+    private void Awake()
+    {
+        dialogueBubbleText = GetComponentInChildren<Text>();
+        xDisplacement = dialogueBubbleText.rectTransform.localPosition.x;
+        generator = dialogueBubbleText.cachedTextGenerator;
+
+        clickableTerms = new List<string>()
+        {
+            "minds",
+            "proportion"
+        };
+    }
+
+    private void OnEnable()
+    {
+        dialogueBubbleText.text = textContents;
+        HighlightClickableTerms();
+    }
+
+    void HighlightClickableTerms()
+    {
+        string newDialogueBubbleText = dialogueBubbleText.text;
+        for (int i = 0; i < clickableTerms.Count; i++)
+        {
+            string clickableTerm = clickableTerms[i];
+            if (!newDialogueBubbleText.Contains(clickableTerm))
+            {
+                Debug.LogError("clickable term not in string: " + clickableTerm);
+                continue;
+            }
+
+            // Can't cache indices here because length/position will update as RTF color code is added.
+
+            int startOfTermIndex = newDialogueBubbleText.IndexOf(clickableTerm) - 1;
+
+            newDialogueBubbleText = newDialogueBubbleText.Insert(startOfTermIndex, "<color=red>");
+
+            int endOfTermIndex = newDialogueBubbleText.IndexOf(clickableTerm) + clickableTerm.Length;
+
+            newDialogueBubbleText = newDialogueBubbleText.Insert(endOfTermIndex, "</color>");
+        }
+
+        dialogueBubbleText.text = newDialogueBubbleText;
+    }   
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        generator = dialogueBubbleText.cachedTextGenerator;
+        Vector2 clickPosition = dialogueBubbleText.transform.worldToLocalMatrix.MultiplyPoint(eventData.position);
+
+        for (int i = 0; i < clickableTerms.Count; i++)
+        {
+            string clickableTerm = clickableTerms[i];
+            if (string.IsNullOrEmpty(clickableTerm)) continue;
+            if (!dialogueBubbleText.text.Contains(clickableTerm))
+            {
+                Debug.Log("clickable term not in string");
+                continue;
+            }
+
+            int termIndex = dialogueBubbleText.text.IndexOf(clickableTerm);
+
+            for (int pos = termIndex; pos < termIndex + clickableTerm.Length; pos++)
+            {
+                Vector2 upperLeft = new Vector2(generator.verts[pos * 4].position.x, generator.verts[pos * 4 + 2].position.y);
+                Vector2 bottomRight = new Vector2(generator.verts[pos * 4 + 2].position.x, generator.verts[pos * 4].position.y);
+
+                bool clickInBounds = clickPosition.x >= upperLeft.x && clickPosition.y >= upperLeft.y && clickPosition.x <= bottomRight.x && clickPosition.y <= bottomRight.y;
+
+                if (clickInBounds)
+                {
+                    Debug.Log("clicked word: " + clickableTerm);
+                    return;
+                }
+
+            }
+        }
+    }
+}
