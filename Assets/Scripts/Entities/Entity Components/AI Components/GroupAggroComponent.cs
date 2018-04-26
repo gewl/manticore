0fe3ce032c1fe2f2a1e34d05d49ff717287c878c
gewl <gilbertwlawson@gmail.com>
2018-04-26 14:14:54 -0400

    added GroupReferenceComponent and infrastructure for multi-entity organisms

--- Assets/Scripts/Entities/Entity Components/AI Components/GroupAggroComponent.cs
+++ Assets/Scripts/Entities/Entity Components/AI Components/GroupAggroComponent.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class GroupAggroComponent : EntityComponent {
 
-    [SerializeField]
-    List<EntityEmitter> entitiesToAggro;
+    List<Transform> entitiesToAggro;
 
     [SerializeField]
     float aggroDelay = 0.1f;
@@ -22,16 +21,17 @@ public class GroupAggroComponent : EntityComponent {
 
     void OnAggro()
     {
+        entitiesToAggro = GetComponent<GroupReferenceComponent>().GetGroup();
         StartCoroutine(AlertGroupMembers());
     }
 
     IEnumerator AlertGroupMembers()
     {
-        foreach (EntityEmitter emitter in entitiesToAggro)
+        foreach (Transform entity in entitiesToAggro)
         {
             Transform currentTarget = (Transform)entityInformation.GetAttribute(EntityAttributes.CurrentTarget);
-            emitter.GetComponent<EntityManagement>().SetAttribute(EntityAttributes.CurrentTarget, currentTarget);
-            emitter.EmitEvent(EntityEvents.Aggro);
+            entity.GetComponent<EntityManagement>().SetAttribute(EntityAttributes.CurrentTarget, currentTarget);
+            entity.GetComponent<EntityEmitter>().EmitEvent(EntityEvents.Aggro);
             yield return new WaitForSeconds(aggroDelay);
         }
     }
---
+++ Assets/Scripts/Entities/Entity Components/Boss Components/GroupReferenceComponent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class GroupReferenceComponent : SerializedMonoBehaviour {
+
+    [SerializeField]
+    List<Transform> groupMembers;
+
+    public List<Transform> GetGroup()
+    {
+        return groupMembers;
+    }
+
+    public Transform GetGroupMember(int index)
+    {
+        if (index < 0 || index > groupMembers.Count)
+        {
+            Debug.LogError("Trying to get out-of-range index from GroupReferenceComponent.");
+            return null;
+        }
+
+        return groupMembers[index];
+    }
+}
