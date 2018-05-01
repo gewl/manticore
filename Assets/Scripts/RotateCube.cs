458b3ed8ef2bddeca4b5bde002fa1e0e1de77a54
gewl <gilbertwlawson@gmail.com>
2018-04-30 21:42:54 -0400

    Building boss body/anims

---
+++ Assets/Scripts/RotateCube.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour {
+
+    [SerializeField]
+    float rotationSpeed = 1f;
+
+	void Update () {
+        float speed = Time.deltaTime * rotationSpeed;
+        transform.Rotate(speed, speed, speed);
+	}
+}
