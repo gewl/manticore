f3de0fc4bc4ad96add02b14cff83775412dee299
gewl <gilbertwlawson@gmail.com>
2018-05-09 18:04:47 -0400

    Added ramp moving mechanism

---
+++ Assets/EnvironmentManipulator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentManipulator : MonoBehaviour, IInteractableObjectController {
+
+    [SerializeField]
+    StatefulObject attachedObject;
+
+    public void RegisterInteraction()
+    {
+        attachedObject.Move();
+    }
+}
---
+++ Assets/StatefulObject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;  
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class StatefulObject : SerializedMonoBehaviour {
+
+    [SerializeField]
+    Vector3 destinationPosition;
+
+    bool hasMoved = false;
+    float timeToMove = 1.0f;
+
+    public void Move()
+    {
+        if (hasMoved)
+        {
+            return;
+        }
+
+        hasMoved = true;
+        Debug.Log("starting coroutine");
+        StartCoroutine(ChangePosition());
+    }
+
+    IEnumerator ChangePosition()
+    {
+        Vector3 initialPosition = transform.localPosition;
+
+        float timeElapsed = 0.0f;
+
+        while (timeElapsed < timeToMove)
+        {
+            float percentageComplete = timeElapsed / timeToMove;
+            float curvedCompletion = GameManager.BelovedSwingCurve.Evaluate(percentageComplete);
+
+            transform.localPosition = Vector3.Lerp(initialPosition, destinationPosition, curvedCompletion);
+
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localPosition = destinationPosition;
+    }
+}
