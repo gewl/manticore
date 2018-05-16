4725aad660831806636828e1fd20e279ab6d5b8d
gewl <gilbertwlawson@gmail.com>
2018-05-15 21:05:30 -0400

    Basic tutorial bub appearing

---
+++ Assets/TutorialController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour {
+
+    float targetPositionOffset = 0f;
+
+    [SerializeField]
+    float transitionTime = 0.5f;
+
+    float resetPositionOffset;
+    RectTransform elementRectTransform;
+
+    float x, height, width;
+
+    private void Awake()
+    {
+        elementRectTransform = transform.GetChild(0).GetComponent<RectTransform>();
+
+        x = elementRectTransform.anchoredPosition.x;
+        height = elementRectTransform.rect.height;
+        width = elementRectTransform.rect.width;
+
+        resetPositionOffset = height * 1.5f;
+
+        StartCoroutine(TransitionIn());
+    }
+
+    IEnumerator TransitionIn()
+    {
+        UpdateOffset(resetPositionOffset);
+
+        float timeElapsed = 0.0f;
+
+        while (timeElapsed < transitionTime)
+        {
+            float percentageComplete = timeElapsed / transitionTime;
+            float curvedPercentageComplete = GameManager.BelovedSwingCurve.Evaluate(percentageComplete);
+
+            float newYOffset = Mathf.Lerp(resetPositionOffset, targetPositionOffset, curvedPercentageComplete);
+
+            UpdateOffset(newYOffset);
+
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        UpdateOffset(targetPositionOffset);
+    }
+
+    void UpdateOffset(float newOffset)
+    {
+        elementRectTransform.anchoredPosition = new Vector3(x, newOffset, 0f);
+    }
+}
