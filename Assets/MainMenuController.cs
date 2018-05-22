5631b2fd9060ef41eaaabc1a1d60c8f4e5696296
gewl <gilbertwlawson@gmail.com>
2018-05-21 22:26:48 -0400

    Main menu working

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+    [SerializeField]
+    GameObject mainMenu;
+    [SerializeField]
+    GameObject settingsPane;
+    [SerializeField]
+    GameObject creditsPane;
+
+    void CloseAllPanes()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+
+    public void OpenMainMenu()
+    {
+        CloseAllPanes();
+
+        mainMenu.SetActive(true);
+    }
+
+    public void OpenSettingsPane()
+    {
+        CloseAllPanes();
+        settingsPane.SetActive(true);
+    }
+
+    public void OpenCreditsPane()
+    {
+        CloseAllPanes();
+        creditsPane.SetActive(true);
+    }
+
+    public void OpenTutorialScene()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+}
