b97ebb406ee097d1bb716872e266da803cb1ea55
gewl <gilbertwlawson@gmail.com>
2018-06-14 00:42:17 -0400

    equip passive tutorial bubs appear in order as you progress

---
+++ Assets/Scripts/Tutorial/EquipPassiveBubSequencer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class EquipPassiveBubSequencer : MonoBehaviour
+{
+    [SerializeField]
+    GameObject dragBub;
+    [SerializeField]
+    GameObject onParryBub;
+    [SerializeField]
+    InventoryMenuController inventoryMenu;
+
+    private void OnEnable()
+    {
+        InventoryController.OnInventoryUpdated += DragBubActivator;
+    }
+
+    private void OnDisable()
+    {
+        InventoryController.OnInventoryUpdated -= DragBubActivator;
+        if (inventoryMenu.OnDraggingHardware != null)
+        {
+            inventoryMenu.OnDraggingHardware -= OnParryBubActivator;
+        }
+    }
+
+    void DragBubActivator(InventoryData inventory)
+    {
+        if (inventory.EquippedActiveHardware[2] == HardwareType.None)
+        {
+            dragBub.SetActive(true);
+            InventoryController.OnInventoryUpdated -= DragBubActivator;
+            inventoryMenu.OnDraggingHardware += OnParryBubActivator;
+        }
+    }
+
+    void OnParryBubActivator(HardwareType hardwareType)
+    {
+        if (hardwareType == HardwareType.Nullify)
+        {
+            onParryBub.SetActive(true);
+            inventoryMenu.OnDraggingHardware -= OnParryBubActivator;
+        }
+    }
+}
